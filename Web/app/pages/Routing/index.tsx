1a9d0e2fb333f3351921dc3fbf193914c455334c
PlETNEV DMITRII <Dmitr45@yandex.ru>
2024-12-07 21:07:31 +0700

    Nav

--- Web/app/pages/Routing/index.tsx
+++ Web/app/pages/Routing/index.tsx
@@ -1,11 +1,11 @@
 'use client'
 import { useAppContext } from "@/app/context/ContextProvider"
 import { pagesType } from '@/app/context/types'
-import { Authorization } from "../Auth";
-import { Registration } from "../Registration";
+import { Authorization } from "@/app/pages/Auth";
+import { Registration } from "@/app/pages/Registration";
 import { None } from "../None/index";
-import { Navigation } from "../Navigation";
-import { Profile } from "../Profile";
+import { Navigation } from "@/app/pages/Navigation";
+import { Profile } from "@/app/pages/Profile";
 
 
 
